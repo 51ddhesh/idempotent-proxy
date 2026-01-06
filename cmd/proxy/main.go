378b003716a8be6fa5f390d95c0a6d2c5bcd4880
siddhesh <173941233+51ddhesh@users.noreply.github.com>
2026-01-06 22:04:38 +0000

    error handling for network failure, TODO: integrate Toxiproxy

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -17,14 +17,27 @@ func main() {
 	}
 
 	service := idempotency.NewService(rdb)
-	targetURL, _ := url.Parse("http://localhost:8081")
+	targetURL, _ := url.Parse("http://localhost:8082")
 
 	proxy := httputil.NewSingleHostReverseProxy(targetURL)
 	proxy.ModifyResponse = service.ResponseHook
 
+	proxy.ErrorHandler = func(w http.ResponseWriter, r *http.Request, e error) {
+		log.Printf("[PROXY]: HTTP/Network Error: %v\n", e)
+		key := r.Header.Get("X-Idempotency-Key")
+
+		if key != "" {
+			log.Printf("[PROXY]: Unlocking %s due to network failure\n", key)
+			rdb.Del(r.Context(), "idem:"+key)
+		}
+
+		w.WriteHeader(http.StatusBadGateway)
+		w.Write([]byte("Bad Gateway: Network Failure\n"))
+	}
+
 	handler := service.Middleware(proxy)
 
-	log.Println("[PROXY]: Running on Port :8080")
+	log.Println("[PROXY]: Backend running on Port :8080")
 	if err := http.ListenAndServe(":8080", handler); err != nil {
 		log.Fatal(err)
 	}
